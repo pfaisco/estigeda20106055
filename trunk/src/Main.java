e86cf4501e1035a5dcd89af9eea6f977b78a51a7
pedrofaisco <pedrofaisco@3351113d-e96e-767e-166c-fdd127260c18>
2011-01-16 23:51:06 +0000

    ja sem, out of bounds error

--- trunk/src/Main.java
+++ trunk/src/Main.java
@@ -13,8 +13,8 @@ public class Main {
 //			img.save("C:\\Users\\Pedro\\Downloads\\t_bw.jpg");
 			TwoPassesConnectedComponents c = new TwoPassesConnectedComponents(img.wRast);
 			c.firstPass();
-//			img.wRast = c.wrOut;
-//			img.save("C:\\Users\\Pedro\\Downloads\\teste_out.jpg");
+			img.wRast = c.wrOut;
+			img.save("C:\\Users\\Pedro\\Downloads\\teste_out.jpg");
 			
 
 	}
