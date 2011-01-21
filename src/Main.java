ed1c22a70cfd2e9201e057e07dbbfd4f65ee66b0
pedrofaisco <pedrofaisco@3351113d-e96e-767e-166c-fdd127260c18>
2011-01-21 03:39:33 +0000

    Funcionando impecavel, excepto para os alicates

--- src/Main.java
+++ src/Main.java
@@ -8,18 +8,19 @@ public class Main {
 	public static void main(String[] args) {
 		
 			ImageOperations img = new ImageOperations();
-			img.open("C:\\Users\\Pedro\\Downloads\\eda\\chave.jpg");
-			img.binarization(180);
+			img.open("C:\\Users\\Pedro\\Downloads\\eda\\paus.jpg");
+			img.binarization(200);
 //			img.save("C:\\Users\\Pedro\\Downloads\\eda\\canetas1bp.bmp");
 			TwoPassesConnectedComponents c = new TwoPassesConnectedComponents(img.wRast);
+			
 			double tempo = System.nanoTime();
 			c.firstPass();
 //			img.save("C:\\Users\\Pedro\\Downloads\\eda\\canetas1fp.bmp");
-//			c.secondPass();
+			c.secondPass();
 			tempo = System.nanoTime() - tempo;
 			System.out.println(tempo*0.000000001+" segundos");
 			img.wRast = c.wrOut;
-			img.save("C:\\Users\\Pedro\\Downloads\\eda\\chave_out.bmp");
+			img.save("C:\\Users\\Pedro\\Downloads\\eda\\paus_out.bmp");
 			
 
 	}
